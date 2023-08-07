ac0bc196dbdfd59d92fbb5f45466fbae549af944
yadj1 <ya240@sussex.ac.uk>
2023-08-07 13:41:04 +0300

    first commit

---
+++ lab7/phpExamples/example1/footer.php
@@ -0,0 +1,3 @@
+       <p>Copyright 2023</p>      
+</body>
+</html>
---
+++ lab8/part_c/application/model/createjson.php
@@ -0,0 +1,23 @@
+<?php
+echo '{ "users":[
+            {
+                "firstName":"Yad",
+                "lastName":"Al-Jaf",
+                "joined": {
+                    "month":"July",
+                    "day":4,
+                    "year":2020
+                }
+            },
+            {
+                "firstName":"Ganza",
+                "lastName":"konari",
+                "joined": {
+                    "month":"January",
+                    "day":12,
+                    "year":2022
+                }
+            }
+    ]}
+	';
+?>
